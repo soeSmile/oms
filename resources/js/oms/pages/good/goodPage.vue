4909773c30523931245e4321d2a1470305d143c3
Smirnov <soe190715@gmail.com>
2022-10-25 18:25:33 +0700

    #PI-13 Хранение и вывод изображений

--- resources/js/oms/pages/good/goodPage.vue
+++ resources/js/oms/pages/good/goodPage.vue
@@ -20,136 +20,157 @@
 
       <div class="content">
 
-        <div class="sp-good sp-flex top left wrap">
-          <div class="block">
-            <div class="sp-flex col">
-              <div class="sp-mb-2">Name</div>
-              <el-input size="large" v-model="good.name" clearable/>
-            </div>
-
-            <div class="sp-flex col sp-mt-4">
-              <div class="sp-mb-2">Brand</div>
-              <el-select size="large"
-                         clearable
-                         filterable
-                         v-model="good.brandId">
-                <el-option v-for="brand in brands"
-                           :value="brand.id"
-                           :label="brand.name"/>
-              </el-select>
-            </div>
-
-            <div class="sp-flex wrap middle">
-              <div class="sp-flex col sp-mr-2 sp-mt-4">
-                <div class="sp-mb-2">Width in box</div>
-                <el-input size="large" v-model="good.widthBox"/>
+        <el-tabs v-model="tabs">
+          <el-tab-pane name="Base">
+            <template #label>
+              <div class="sp-flex middle">
+                <i class='bx bx-list-ul sp-mr-2'/>
+                <span>Common data</span>
               </div>
-
-              <div class="sp-flex col sp-mr-2 sp-mt-4">
-                <div class="sp-mb-2">Height in box</div>
-                <el-input size="large" v-model="good.heightBox"/>
-              </div>
-
-              <div class="sp-flex col sp-mr-2 sp-mt-4">
-                <div class="sp-mb-2">Length in box</div>
-                <el-input size="large" v-model="good.lengthBox"/>
-              </div>
-
-              <div class="sp-flex col sp-mr-2 sp-mt-4">
-                <div class="sp-mb-2">Gross weight</div>
-                <el-input size="large" v-model="good.weightGross"/>
-              </div>
-
-              <div class="sp-flex col sp-mt-4">
-                <div class="sp-mb-2">Volume</div>
-                <el-input size="large" v-model="good.volume"/>
-              </div>
-            </div>
-
-            <div class="sp-flex col sp-mt-4">
-              <div class="sp-mb-2">Pledge goods</div>
-              <el-switch v-model="good.deposit"/>
-            </div>
-          </div>
-
-          <div class="block">
-            <div class="sp-flex col">
-              <div class="sp-mb-2">Category</div>
-              <el-select size="large"
-                         clearable
-                         filterable
-                         multiple
-                         collapse-tags
-                         collapse-tags-tooltip
-                         v-model="good.category">
-                <el-option v-for="brand in categories"
-                           :value="brand.id"
-                           :label="brand.name"/>
-              </el-select>
-            </div>
-            <div class="sp-flex col sp-mt-2">
-              <div class="sp-flex middle sp-p-1"
-                   v-for="(val,key) in good.category">
-                <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeCategory(key)"/>
-                <span>{{ viewCategoryName(val) }}</span>
-              </div>
-            </div>
-
-            <div class="sp-w-100 sp-flex wrap sp-mt-6">
+            </template>
+            <div class="sp-mt-6 sp-good sp-flex top left wrap">
               <div class="block">
-                <div class="sp-mb-2">Catalogue number</div>
-                <div class="sp-flex middle">
-                  <el-input size="large" v-model="number"/>
-                  <ui-button icon="bx bx-plus" @click="addNumber"/>
+                <div class="sp-flex col">
+                  <div class="sp-mb-2">Name</div>
+                  <el-input size="large" v-model="good.name" clearable/>
                 </div>
-                <div class="sp-flex middle sp-p-1 sp-mt-2"
-                     v-for="(val,key) in good.number">
-                  <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeNumber(key)"/>
-                  <span>{{ val }}</span>
-                </div>
-              </div>
-              <div class="block">
-                <div class="sp-mb-2">ОЕ/ОЕМ</div>
-                <div class="sp-flex middle">
-                  <el-input size="large" v-model="oe"/>
-                  <ui-button icon="bx bx-plus" @click="addOe"/>
-                </div>
-                <div class="sp-flex middle sp-p-1 sp-mt-2"
-                     v-for="(val,key) in good.oe">
-                  <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeOe(key)"/>
-                  <span>{{ val }}</span>
-                </div>
-              </div>
-            </div>
 
-            <div class="sp-w-100 sp-flex wrap sp-mt-6">
-              <div class="block">
-                <div class="sp-mb-2">TNVED</div>
-                <div class="sp-flex middle">
-                  <el-input size="large" v-model="tnved"/>
-                  <ui-button icon="bx bx-plus" @click="addOTnved"/>
+                <div class="sp-flex col sp-mt-4">
+                  <div class="sp-mb-2">Brand</div>
+                  <el-select size="large"
+                             clearable
+                             filterable
+                             v-model="good.brandId">
+                    <el-option v-for="brand in brands"
+                               :value="brand.id"
+                               :label="brand.name"/>
+                  </el-select>
                 </div>
-                <div class="sp-flex middle sp-p-1 sp-mt-2"
-                     v-for="(val,key) in good.tnved">
-                  <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeTnved(key)"/>
-                  <span>{{ val }}</span>
+
+                <div class="sp-flex wrap middle wide">
+                  <div class="sp-flex col sp-mr-2 sp-mt-4">
+                    <div class="sp-mb-2">Width in box</div>
+                    <el-input size="large" v-model="good.widthBox"/>
+                  </div>
+
+                  <div class="sp-flex col sp-mr-2 sp-mt-4">
+                    <div class="sp-mb-2">Height in box</div>
+                    <el-input size="large" v-model="good.heightBox"/>
+                  </div>
+
+                  <div class="sp-flex col sp-mr-2 sp-mt-4">
+                    <div class="sp-mb-2">Length in box</div>
+                    <el-input size="large" v-model="good.lengthBox"/>
+                  </div>
+
+                  <div class="sp-flex col sp-mr-2 sp-mt-4">
+                    <div class="sp-mb-2">Gross weight</div>
+                    <el-input size="large" v-model="good.weightGross"/>
+                  </div>
+
+                  <div class="sp-flex col sp-mt-4">
+                    <div class="sp-mb-2">Volume</div>
+                    <el-input size="large" v-model="good.volume"/>
+                  </div>
+                </div>
+
+                <div class="sp-flex col sp-mt-4">
+                  <div class="sp-mb-2">Pledge goods</div>
+                  <el-switch v-model="good.deposit"/>
                 </div>
               </div>
+
               <div class="block">
-                <div class="sp-mb-2">HS Code</div>
-                <div class="sp-flex middle">
-                  <el-input size="large" v-model="hscode"/>
-                  <ui-button icon="bx bx-plus" @click="addHsCode"/>
+                <div class="sp-flex col">
+                  <div class="sp-mb-2">Category</div>
+                  <el-select size="large"
+                             clearable
+                             filterable
+                             multiple
+                             collapse-tags
+                             collapse-tags-tooltip
+                             v-model="good.category">
+                    <el-option v-for="brand in categories"
+                               :value="brand.id"
+                               :label="brand.name"/>
+                  </el-select>
                 </div>
-                <div class="sp-flex middle sp-p-1 sp-mt-2"
-                     v-for="(val,key) in good.hscode">
-                  <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeHsCode(key)"/>
-                  <span>{{ val }}</span>
+                <div class="sp-flex col sp-mt-2">
+                  <div class="sp-flex middle sp-p-1"
+                       v-for="(val,key) in good.category">
+                    <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeCategory(key)"/>
+                    <span>{{ viewCategoryName(val) }}</span>
+                  </div>
+                </div>
+
+                <div class="sp-w-100 sp-flex wrap wide sp-mt-6">
+                  <div class="block">
+                    <div class="sp-mb-2">Catalogue number</div>
+                    <div class="sp-flex middle">
+                      <el-input size="large" v-model="number"/>
+                      <ui-button icon="bx bx-plus" @click="addNumber"/>
+                    </div>
+                    <div class="sp-flex middle sp-p-1 sp-mt-2"
+                         v-for="(val,key) in good.number">
+                      <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeNumber(key)"/>
+                      <span>{{ val }}</span>
+                    </div>
+                  </div>
+                  <div class="block">
+                    <div class="sp-mb-2">ОЕ/ОЕМ</div>
+                    <div class="sp-flex middle">
+                      <el-input size="large" v-model="oe"/>
+                      <ui-button icon="bx bx-plus" @click="addOe"/>
+                    </div>
+                    <div class="sp-flex middle sp-p-1 sp-mt-2"
+                         v-for="(val,key) in good.oe">
+                      <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeOe(key)"/>
+                      <span>{{ val }}</span>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="sp-w-100 sp-flex wrap wide sp-mt-6">
+                  <div class="block">
+                    <div class="sp-mb-2">TNVED</div>
+                    <div class="sp-flex middle">
+                      <el-input size="large" v-model="tnved"/>
+                      <ui-button icon="bx bx-plus" @click="addOTnved"/>
+                    </div>
+                    <div class="sp-flex middle sp-p-1 sp-mt-2"
+                         v-for="(val,key) in good.tnved">
+                      <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeTnved(key)"/>
+                      <span>{{ val }}</span>
+                    </div>
+                  </div>
+                  <div class="block">
+                    <div class="sp-mb-2">HS Code</div>
+                    <div class="sp-flex middle">
+                      <el-input size="large" v-model="hscode"/>
+                      <ui-button icon="bx bx-plus" @click="addHsCode"/>
+                    </div>
+                    <div class="sp-flex middle sp-p-1 sp-mt-2"
+                         v-for="(val,key) in good.hscode">
+                      <i class='bx bx-x sp-mr-2 sp-link sp-danger' @click="removeHsCode(key)"/>
+                      <span>{{ val }}</span>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
+          </el-tab-pane>
+          <el-tab-pane name="Images" :disabled="!good.id">
+            <template #label>
+              <div class="sp-flex middle">
+                <i class='bx bx-image-alt sp-mr-2'/>
+                <span>Images</span>
+              </div>
+            </template>
+            <div class="sp-mt-6">
+              Images
+            </div>
+          </el-tab-pane>
+        </el-tabs>
 
       </div>
 
@@ -164,6 +185,7 @@ import { onMounted, ref } from 'vue'
 import { error, success } from '../../../helper/reponse'
 import { useRoute, useRouter } from 'vue-router'
 
+const tabs = ref('Base')
 const loading = ref(false)
 const router = useRouter()
 const route = useRoute()
