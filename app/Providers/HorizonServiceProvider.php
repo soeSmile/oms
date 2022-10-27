44ae6447b4a8eb4e4fe25f5da61ea35d63e971c7
Smirnov <oleg@grademarc.ru>
2022-10-27 12:57:44 +0700

    add horizon

---
+++ app/Providers/HorizonServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Gate;
+use Laravel\Horizon\Horizon;
+use Laravel\Horizon\HorizonApplicationServiceProvider;
+
+class HorizonServiceProvider extends HorizonApplicationServiceProvider
+{
+    /**
+     * @return void
+     */
+    public function boot(): void
+    {
+        parent::boot();
+
+        // Horizon::routeSmsNotificationsTo('15556667777');
+        // Horizon::routeMailNotificationsTo('example@example.com');
+        // Horizon::routeSlackNotificationsTo('slack-webhook-url', '#channel');
+
+        // Horizon::night();
+    }
+
+    /**
+     * @return void
+     */
+    protected function gate(): void
+    {
+        Gate::define('viewHorizon', static function ($user) {
+            return $user->email === 'admin@yandex.ru';
+        });
+    }
+}
