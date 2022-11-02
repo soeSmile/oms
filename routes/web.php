5a26ff829e8d08c7552d9da1b9c11dff57dcb9ed
Smirnov <oleg@grademarc.ru>
2022-11-02 18:26:04 +0700

    #PI-3 Создать дев сервер

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ Route::get('/login', [IndexController::class, 'login'])->name('web.login');
 
 Route::group(['middleware' => 'auth'], static function () {
     Route::get('/oms', [IndexController::class, 'oms'])->name('web.oms');
-    Route::get('/oms/{any}', [IndexController::class, 'oms'])->where('any', '.*')->name('web.oms');
+    Route::get('/oms/{any}', [IndexController::class, 'oms'])->where('any', '.*')->name('web.oms.all');
 
     Route::get('/good/{id}/img/{image}', [GoodController::class, 'showImage'])->name('good.image.show');
 });
